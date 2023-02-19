d63dd87b57f1646a2312b4e73bd0532b500be2f5
Blue Gloomy <wy1762456843@gmail.com>
2023-02-20 00:34:24 +0800

    Initial commit

---
+++ src/main/java/com/wangyuan/springboot_testbylinux/SpringBootTestByLinuxApplication.java
@@ -0,0 +1,13 @@
+package com.wangyuan.springboot_testbylinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestByLinuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestByLinuxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangyuan/springboot_testbylinux/SpringBootTestByLinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangyuan.springboot_testbylinux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestByLinuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
